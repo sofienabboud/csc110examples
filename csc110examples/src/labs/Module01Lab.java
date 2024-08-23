d25cfb11ebf80e0ed6498ebe3b685cf46fab7e0c
sofienabboud <snabboud1@gmail.com>
2024-08-22 22:42:58 -0700

    Update Module01Lab.java

--- csc110examples/src/labs/Module01Lab.java
+++ csc110examples/src/labs/Module01Lab.java
@@ -12,8 +12,10 @@ public class Module01Lab {
 		System.out.println();
 		
 		// Print "Hello, CSC110", ending with a newline
+		Sytem.out.println("Hello, CSC110"); ending with a newline
 
 		// Print "Effort creates opportunity" inside a box on 3 lines
+		system.out.println("Effort creates opportunity")inside a box on 3 lines;
 
     }
     
